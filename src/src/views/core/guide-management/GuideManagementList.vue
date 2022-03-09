b5912945ea98c5fb9099511851557959f822b780
mpdev2311 <marcospdev2311gmail.com>
2022-03-09 15:20:02 -0300

    telas Gestao de Guias

---
+++ src/src/views/core/guide-management/GuideManagementList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <GuideManagementList/>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import GuideManagementList from "../../../components/layouts/guide-management/GuideManagementList.vue"
+
+export default defineComponent({
+    components:{
+        GuideManagementList
+    }
+})
+</script>
